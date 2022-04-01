6f78ac59449258cfcddaee9496a43e4217499fee
Aryan-Nigam <aryannigam032@gmail.com>
2022-04-01 20:14:53 +0530

    route init

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function App() {
     <div className="App">
       <Router>
         <Routes>
-        <Route path="/" element={<Todo/>}/>
+        <Route path="/To-do-App" element={<Todo/>}/>
         </Routes>
       </Router>
     </div>
